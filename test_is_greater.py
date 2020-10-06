4ff4b8c3a5f7d406799d7c6ac7f6e6ec2dcd9065
Vincent Ricci <vricci@cisco.com>
2020-10-06 07:26:34 -0700

    Fixed return value False -> result

--- test_is_greater.py
+++ test_is_greater.py
@@ -12,23 +12,29 @@ def publish_result(test):
         print(f'{test.__name__}: {result}')
     return result
 
+## Note that all the decorated test functions take no
+## argument and return an array
+## value =  [is_greater(x,y), <bool>]
+
 @publish_result
 def test_true_when_greater():
     result = [is_greater(5, 4), True]
     return result
 
+
 @publish_result
 def test_false_when_smaller():
     result = [is_greater(4, 5), False]
-    return False
+    return result
+
 
 @publish_result
 def test_false_when_equal():
     result = [is_greater(5, 5), False]
     return result
 
+
 if __name__ == "__main__":
     test_true_when_greater()
     test_false_when_smaller()
     test_false_when_equal()
-
